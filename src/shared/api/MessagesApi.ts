0bf58c6c6e07ef90ebbf0252dcf10b89e5b387c3
Egor Barkalov <ostrovok125@yandex.ru>
2023-10-24 14:02:28 +0300

    refactoring #2

--- src/shared/api/MessagesApi.ts
+++ src/shared/api/MessagesApi.ts
@@ -68,8 +68,8 @@ export class MessagesApi {
 
 	private setOpenListener() {
 		this.socket?.addEventListener(MessagesEvents.Open, () => {
-			this._isConnectionOpen = true;
 			this.getOldMessages();
+			this._isConnectionOpen = true;
 		});
 	}
 
@@ -101,6 +101,17 @@ export class MessagesApi {
 		this.socket?.close();
 	}
 
+	private onConnectionIsReady(onReadyCallback: () => void, delay: number = WAIT_CONNECTION_DELAY) {
+		if (this.socket?.readyState === SocketReadyState.Open) {
+			this._isConnectionOpen = true;
+			onReadyCallback();
+		} else {
+			setTimeout(() => {
+				this.onConnectionIsReady(onReadyCallback, delay);
+			}, delay);
+		}
+	}
+
 	shouldResetInstance(newProps: MessagesApiProps) {
 		const { chatId, token, userId } = newProps;
 		return this._chatId !== chatId || this._token !== token || this._userId !== userId;
@@ -113,6 +124,10 @@ export class MessagesApi {
 	sendMessage(message: string, type: string = MessagesTypes.Message) {
 		if (this._isConnectionOpen) {
 			this.socket?.send(JSON.stringify({ content: message, type }));
-		} 
+		} else {
+			this.onConnectionIsReady(() => {
+				this.socket?.send(JSON.stringify({ content: message, type }));
+			});
+		}
 	}
 }
